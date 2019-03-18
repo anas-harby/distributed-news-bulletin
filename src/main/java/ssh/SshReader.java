828b249f9575281dc22379200ea4c52847defa8f
anas.harby <anas.harby@gmail.com>
2019-03-18 22:42:23 +0200

    Implement  a basic version of a thread dispatcher, and ssh runnables

---
+++ src/main/java/ssh/SshReader.java
@@ -0,0 +1,13 @@
+package ssh;
+
+public class SshReader extends SshRunnable {
+    public SshReader(String user, String host, int port, int numberOfAccesses) throws SshConnection.SshException {
+        super(user, host, port, numberOfAccesses);
+    }
+
+    @Override
+    public void executeCommand() throws SshConnection.SshException {
+        //TODO: figure out what command to run.
+        super.sshConnection.runCommand("RUN CLIENT --READ MODE");
+    }
+}
